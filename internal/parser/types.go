082e36c03997c6d67bbb1b66d20f06c445e99e30
leo.rolland <leo.rolland@ovhcloud.com>
2023-11-09 22:01:26 +0100

    Add Method type, use google/cmp

--- internal/parser/types.go
+++ internal/parser/types.go
@@ -5,11 +5,22 @@ type (
 		Type Type
 
 		Attributes []Attribute
+		Methods    []Method
 	}
 
 	Attribute struct {
 		Name string
 		Type Type
+
+		Comments []string
+	}
+
+	Method struct {
+		Name              string
+		Params            []Type
+		Returns           []Type
+		IsPointerReceiver bool
+		IsExported        bool
 
 		Comments []string
 	}
