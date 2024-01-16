a96b51893c24690a212fa3f1b196f23628bfa55a
Joffref <mariusjoffre@gmail.com>
2024-01-16 21:47:36 +0100

    Fix CI
    Signed-off-by: Joffref <mariusjoffre@gmail.com>

--- examples/2_getters/main.go
+++ examples/2_getters/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/Joffref/genz/genz/cli"
 	"github.com/Joffref/genz/genz/models"
 	"io"
-	"slices"
 )
 
 func main() {
@@ -16,7 +15,7 @@ func MyCustomGenerator(w io.Writer, parsedElement models.ParsedElement) error {
 	code := genz.NewCode(w, parsedElement.PackageName)
 
 	for _, attribute := range parsedElement.Attributes {
-		if slices.Contains(attribute.Comments, "+getter") {
+		if Contains(attribute.Comments, "+getter") {
 			code.WithDeclarations(
 				genz.Function("Get"+attribute.Name).
 					WithReceiver("v", parsedElement.Type.InternalName, false).
@@ -26,4 +25,13 @@ func MyCustomGenerator(w io.Writer, parsedElement models.ParsedElement) error {
 		}
 	}
 	return code.Generate()
+}
+
+func Contains(slice []string, element string) bool {
+	for _, e := range slice {
+		if e == element {
+			return true
+		}
+	}
+	return false
 }
--- go.mod
+++ go.mod
@@ -3,22 +3,12 @@ module github.com/Joffref/genz
 go 1.21
 
 require (
-	github.com/Masterminds/sprig/v3 v3.2.3
 	github.com/google/go-cmp v0.5.9
 	golang.org/x/tools v0.14.0
 )
 
 require (
-	github.com/Masterminds/goutils v1.1.1 // indirect
-	github.com/Masterminds/semver/v3 v3.2.1 // indirect
-	github.com/google/uuid v1.4.0 // indirect
-	github.com/huandu/xstrings v1.4.0 // indirect
-	github.com/imdario/mergo v0.3.16 // indirect
-	github.com/mitchellh/copystructure v1.2.0 // indirect
-	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
-	github.com/spf13/cast v1.5.1 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect (used for testing purposes)
 	golang.org/x/mod v0.13.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
